0c276d1d5bc8315321a9ba075557b0e15f98a4f6
Rafail <rafail.dev@gmail.com>
2024-08-21 07:26:56 +0000

    t3 improve

--- ch8_common_collections/src/t3_employees.rs
+++ ch8_common_collections/src/t3_employees.rs
@@ -1,6 +1,11 @@
 use std::collections::HashMap;
 use std::io;
 
+enum Result {
+    IsQuit,
+    Message(Vec<String>),
+}
+
 #[allow(dead_code)]
 fn main() {
     let mut db: HashMap<String, String> = HashMap::new();
@@ -12,51 +17,65 @@ fn main() {
             .expect("Failed to read line")
             .expect("Failed to parse line");
 
-        let s = command.split_whitespace().map(|s| s).collect::<Vec<&str>>();
+        let s = command.split_whitespace().collect::<Vec<&str>>();
 
-        match s.as_slice() {
-            ["quit"] => {
-                println!("Bye, bye");
+        let result: Result = match s.as_slice() {
+            ["quit"] => Result::IsQuit,
+            another => Result::Message(match another {
+                ["all"] => {
+                    let mut vec: Vec<(&String, &String)> = db.iter().map(|(e, d)| (d, e)).collect();
+
+                    vec.sort();
+
+                    let result = vec.iter().map(|(d, e)| format!("{}\t{}", d, e));
+
+                    std::iter::once(format!("All Employyers"))
+                        .chain(result)
+                        .collect::<Vec<String>>()
+                }
+                [department] => {
+                    let mut vec: Vec<(&String, &String)> = db
+                        .iter()
+                        .filter(|(_, d)| d == department)
+                        .map(|(e, d)| (d, e))
+                        .collect();
+
+                    vec.sort();
+
+                    let result = vec.iter().map(|(d, e)| format!("{}\t{}", d, e));
+
+                    std::iter::once(format!("Employyers from {}", department))
+                        .chain(result)
+                        .collect::<Vec<String>>()
+                }
+
+                ["add", employee, "to", department] => {
+                    db.insert(employee.to_string(), department.to_string());
+
+                    vec![format!("{} added to {}", employee, department)]
+                }
+
+                ["fire", employee] => match db.remove(&employee.to_string()) {
+                    Some(department) => {
+                        vec![format!("{} fired from P{}", employee, department)]
+                    }
+                    None => {
+                        vec![format!("Employee not found")]
+                    }
+                },
+                _ => vec![command],
+            }),
+        };
+
+        match result {
+            Result::IsQuit => {
+                println!("Bye, Bye");
                 break;
             }
-            ["all"] => {
-                let mut vec: Vec<(&String, &String)> = db.iter().map(|(e, d)| (d, e)).collect();
-
-                vec.sort();
-
-                vec.iter().for_each(|(d, e)| println!("{}\t{}", d, e));
-            }
-            [department] => {
-                let mut vec: Vec<(&String, &String)> = db
-                    .iter()
-                    .filter(|(_, d)| d == department)
-                    .map(|(e, d)| (d, e))
-                    .collect();
-
-                vec.sort();
-
-                vec.iter().for_each(|(d, e)| println!("{}\t{}", d, e));
-            }
-
-            ["Add", employee, "to", department] => {
-                db.insert(employee.to_string(), department.to_string());
-
-                println!("{} added to {}", employee, department);
-            }
-
-            ["Fire", employee] => match db.remove(*employee) {
-                Some(department) => {
-                    println!("{} fired from P{}", employee, department)
-                }
-                None => {
-                    println!("Employee not found")
-                }
-            },
-
-            _ => {
-                println!("{:?}", s);
-            }
+            Result::Message(s) => s
+                .iter()
+                .chain(std::iter::once(&String::new()))
+                .for_each(|s| println!("{}", s)),
         }
-        println!();
     }
 }
