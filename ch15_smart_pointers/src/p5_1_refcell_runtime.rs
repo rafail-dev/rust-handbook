0192466a8c3216036145ce818fa19066b6504d2b
raf <rafail.dev@gmail.com>
2025-01-20 20:01:23 +0000

    refcell snip improve

--- ch15_smart_pointers/src/p5_1_refcell_runtime.rs
+++ ch15_smart_pointers/src/p5_1_refcell_runtime.rs
@@ -2,13 +2,55 @@
 use std::cell::RefCell;
 
 fn main() {
+    // {
+    //     let s = String::from("hello");
+    //     let immut_ref = &s;
+    //     let mut mut_ref = &mut &s; // compile error
+    //     println!("{}", immut_ref);
+    // }
+
     {
         // runtime panic
         // already borrowed: BorrowMutError
         let v = RefCell::new(1);
         let immut_ref = v.borrow();
-        let mut_ref = v.borrow_mut();
-        *v.borrow_mut() += 1;
+        let mut mut_ref = v.borrow_mut();
+        *mut_ref += 1;
         println!("{}", *immut_ref);
+    }
+
+    {
+        // runtime panic
+        // already borrowed: BorrowMutError
+        let v = RefCell::new(1);
+        let immut_ref = v.borrow();
+        println!("{}", *immut_ref);
+
+        let mut mut_ref = v.borrow_mut();
+        *mut_ref += 1;
+        println!("{}", *mut_ref);
+    }
+
+    {
+        let v = RefCell::new(1);
+        {
+            let immut_ref = v.borrow();
+            println!("{}", *immut_ref);
+        }
+
+        let mut mut_ref = v.borrow_mut();
+        *mut_ref += 1;
+        println!("{}", *mut_ref);
+    }
+
+    {
+        let v = RefCell::new(1);
+        let immut_ref = v.borrow();
+        println!("{}", *immut_ref);
+        drop(immut_ref);
+
+        let mut mut_ref = v.borrow_mut();
+        *mut_ref += 1;
+        println!("{}", mut_ref);
     }
 }
